c12fc07ad889543c21fe47ea01f51be7a0313470
David Kim <davidkim120302@gmail.com>
2019-12-15 13:03:54 -0500

    Did the append function

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    let itemArray = ["Find Mike", "Find MIlk", "Find my iphone"]
+    var itemArray = ["Find Mike", "Find MIlk", "Find my iphone"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +46,35 @@ class ToDoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    // Mark - Add New items
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert )
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once the user clicks the Add Item button on our UIAlert
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = "Team name"
+                textField = alertTextField
+            }
+        
+            alert.addAction(action)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+    
+        }
+        
+    }
 
 
-}
 
